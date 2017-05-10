3e384fc470d1bc15fd03558046898f7dc07a77c0
erings <erings@163.com>
2017-05-10 22:07:03 +0800

    123

---
+++ fnjavawebdemo/src/main/java/AppStart.java
@@ -0,0 +1,10 @@
+/**
+ * Created by charles on 2017/5/10.
+ */
+import
+public class AppStart {
+    public static void main(String[]args){
+        System.out.println("hello world");
+        RestTemplate rt = new RestTemplate();
+    }
+}
